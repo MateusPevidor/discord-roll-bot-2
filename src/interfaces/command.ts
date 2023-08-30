de1bcfb819fe5c8919b536e8dbebbb0d7b011269
MateusPevidor <mateus-pevidor@hotmail.com>
2023-08-30 03:07:53 -0300

    add: subcommands support

--- src/interfaces/command.ts
+++ src/interfaces/command.ts
@@ -1,15 +1,21 @@
 
+import { ChatInputCommandInteraction } from 'discord.js';
 import { InteractionResponse } from 'discord.js';
 import { Interaction, SlashCommandBuilder } from 'discord.js';
 
 abstract class ICommand {
 
   command: SlashCommandBuilder;
+  protected subCommandMap: {
+    [name: string]: (interaction: ChatInputCommandInteraction) => Promise<InteractionResponse<boolean>>
+  };
 
   constructor(name: string, description: string) {
     this.command = new SlashCommandBuilder()
       .setName(name)
       .setDescription(description);
+
+    this.subCommandMap = {};
   }
 
   abstract execute(interaction: Interaction): Promise<InteractionResponse<boolean>>;
